f0da0a2572d5fc7c008de08bf3b0d6095abb6728
seonghye0n <seonghye0n@kakao.com>
2024-11-11 21:35:50 +0900

    chore : Initial Commit

---
+++ src/main/java/com/github/seonghye0n/bot/Main.java
@@ -0,0 +1,9 @@
+package com.github.seonghye0n.bot;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+	public static void main(String[] args) {
+		JDABuilder.createDefault("").build();
+	}
+}
